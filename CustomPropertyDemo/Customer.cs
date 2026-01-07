ab8e3549023afbf455ad4815aef9c3c8131ef293
Manish Mamnani <mkm30882@gmail.com>
2026-01-07 09:26:54 +0530

    Day 11

---
+++ CustomPropertyDemo/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomPropertyDemo
+{
+    class Customer
+    {
+        List<Orders> orderList;
+
+        public Customer()
+        {
+            orderList = new List<Orders>();
+        }
+
+        public int CustID { get; set; }
+        public string Name { get; set; }
+        public Address BillingAddr { get; set; }
+        public Address ShippingAddr { get; set; }
+
+        public List<Orders> MyOrders
+        {
+            get
+            {
+                return orderList;
+            }
+            set
+            {
+                orderList = value;
+            }
+        }
+
+    }
+}
---
+++ CustomPropertyDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace CustomPropertyDemo
+{
+    //struct Customer1 //value type(no memory allocation on heap) in Csharp
+    //    //structure will never carry destructor
+    //{
+
+    //    //public int ID { get; set; } = 0;
+    //    //public string Name { get; set; }
+    //}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //init customer object
+
+            Customer custObj = new Customer();
+
+            custObj.CustID = 101;
+            custObj.Name = "Alok";
+
+            //init customer object
+            custObj.ShippingAddr = new Address();
+
+            custObj.ShippingAddr.FlatNo = 1802;
+            custObj.ShippingAddr.BuildingName = "Sky Tower";
+            custObj.ShippingAddr.Street = "Lane 1";
+            custObj.ShippingAddr.Locality = "Wakad";
+            custObj.ShippingAddr.City = "Mumbai";
+
+            //1 customer have many orders
+            custObj.MyOrders = new List<Orders>()
+            {
+                new Orders{ OrderID=1124,OrderDate=new DateTime(2001,12,22),Amount=14000},
+                new Orders{ OrderID=2076,OrderDate=new DateTime(2002,1,15),Amount=24000},
+                new Orders{ OrderID=8540,OrderDate=new DateTime(2004,12,05),Amount=18000},
+                new Orders{ OrderID=1196,OrderDate=new DateTime(2007,08,23),Amount=32000},
+                new Orders{ OrderID=3251,OrderDate=new DateTime(2012,11,21),Amount=10000},
+
+            };
+        }
+    }
+}
