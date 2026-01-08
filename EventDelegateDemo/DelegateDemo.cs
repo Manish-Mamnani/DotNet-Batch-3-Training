ed1cc7351a89b4f9daa99a9cda91aace23f0dd58
Manish-Mamnani <119027516+Manish-Mamnani@users.noreply.github.com>
2026-01-08 09:40:06 +0530

    Delegate

---
+++ EventDelegateDemo/DelegateDemo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventDelegateDemo
+{
+    //MultiCast Delegate
+    public delegate void GreetMsg(string msg);
+
+    //UniCast Delegate
+    public delegate int Calcuation(int num1, int num2);
+
+    class Hindi
+    {
+        public void WelcomeMsg(string userName)
+        {
+            Console.WriteLine("Suprabhat"+userName);
+        }
+
+    }
+
+    class Tamil
+    {
+        public void WelcomeMsg(string userName)
+        {
+            Console.WriteLine("Vanakkam" + userName);
+        }
+    }
+
+    class Telugu
+    {
+        public void WelcomeMsg(string userName)
+        {
+            Console.WriteLine("Namaskaram" + userName);
+        }
+
+    }
+
+    class English
+    {
+        public void WelcomeMsg(string userName)
+        {
+            Console.WriteLine("Good Morning" + userName);
+        }
+
+    }
+    class Marathi
+    {
+        public void WelcomeMsg(string userName)
+        {
+            Console.WriteLine("Suprabhat" + userName);
+        }
+
+    }
+    public class DelegateDemo
+    {
+        public static void DelegateDemoMain()
+        {
+            Tamil tObj = new Tamil();
+            GreetMsg GreetInTamil = new GreetMsg(tObj.WelcomeMsg);
+
+            GreetInTamil("Alok");
+
+        }
+
+    }
+}
