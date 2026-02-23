703d1612c84455b00c2f1ed2aa011b6d7c61998d
Manish Mamnani <mkm30882@gmail.com>
2026-02-23 15:09:41 +0530

    Repo Update

---
+++ MVCWebAppDemo/Controllers/DummyController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWebAppDemo.Controllers
+{
+    public class DummyController : Controller
+    {
+        // GET: Dummy
+        //Action Method
+        public ActionResult Index()
+        {
+            List<string> nameList = new List<string>()
+            {
+                "Alok","Rajat","Mahi"
+            };
+
+            ViewBag.Names = nameList;
+
+            return View();
+        }
+    }
+}
